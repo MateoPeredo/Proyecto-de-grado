26b9ba7e2a8355e896a473e9d0b738906b6955ca
TANHEHANZ <hantach10@gmail.com>
2025-01-29 23:01:12 -0400

    feat: create filter table with select element

--- front/src/presentation/shared/global/table.tsx
+++ front/src/presentation/shared/global/table.tsx
@@ -1,7 +1,9 @@
+import React, { useState } from "react";
 import {
   useReactTable,
   getCoreRowModel,
   ColumnDef,
+  getFilteredRowModel,
 } from "@tanstack/react-table";
 import { ICON_SEARCH } from "./icons";
 import { FilterButton } from "./button";
@@ -10,36 +12,81 @@ interface PropsTable<T> {
   data: T[];
   columns: ColumnDef<T, any>[];
 }
+
 export const Table = <T extends object>({ data, columns }: PropsTable<T>) => {
+  const [globalFilter, setGlobalFilter] = useState("");
+  const [filterBy, setFilterBy] = useState<string | null>(null);
+
+  // Filtrar los datos basados en el valor de búsqueda y la columna seleccionada
+  const filteredData = filterBy
+    ? data.filter((row) =>
+        String(row[filterBy as keyof T])
+          .toLowerCase()
+          .includes(globalFilter.toLowerCase())
+      )
+    : data;
+
+  // Usar los datos filtrados en la tabla
   const table = useReactTable({
-    data,
+    data: filteredData, // Pasar los datos filtrados aquí
     columns,
+    state: {
+      globalFilter,
+    },
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
   });
 
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGlobalFilter(event.target.value);
+  };
+
+  const handleFilterByChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setFilterBy(event.target.value);
+  };
+
   return (
-    <section className=" w-full h-full rounded-md overflow-hidden  shadow-md p-4 bg-white">
-      <h5 className="font-bold my-4 text-lg ">Name Table</h5>
-      <section className=" border  rounded-md ">
-        <article className="flex justify-between  p-4">
-          <FilterButton>Limpiar</FilterButton>
-          <div className=" border-2 flex justify-center items-center  focus-within:border-primary rounded-md p-2 gap-2 hover:border-primary">
+    <section className="w-full h-full rounded-md overflow-hidden shadow-md p-4 bg-white">
+      <h5 className="font-bold my-4 text-lg">Name Table</h5>
+      <section className="border rounded-md">
+        <article className="flex justify-between p-4">
+          <FilterButton onClick={() => setGlobalFilter("")}>
+            Limpiar
+          </FilterButton>
+          <div className="border-2 flex justify-center items-center focus-within:border-primary rounded-md p-2 gap-2 hover:border-primary">
             {ICON_SEARCH}
             <input
               type="search"
-              className=" rounded-md placeholder:text-sm placeholder:font-light outline-none"
+              className="rounded-md placeholder:text-sm placeholder:font-light outline-none"
               placeholder="Buscar"
+              value={globalFilter}
+              onChange={handleFilterChange}
             />
+            <select
+              className="rounded-md p-1 outline-none"
+              onChange={handleFilterByChange}
+              value={filterBy || ""}
+            >
+              <option value="">Seleccionar columna</option>
+              {columns.map((column) => (
+                <option key={column.id} value={column.id}>
+                  {String(column.header)}
+                </option>
+              ))}
+            </select>
           </div>
         </article>
-        <table className=" w-full h-full ">
+        <table className="w-full h-full">
           <thead>
             {table.getHeaderGroups().map((headerGroup) => (
               <tr key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
                   <th
                     key={header.id}
-                    className="py-4 px-2 text-start text-sm   border"
+                    className="py-4 px-2 text-start text-sm border"
                   >
                     {header.isPlaceholder
                       ? null
@@ -57,7 +104,7 @@ export const Table = <T extends object>({ data, columns }: PropsTable<T>) => {
                 {row.getVisibleCells().map((cell) => (
                   <td
                     key={cell.id}
-                    className="py-4  text-start px-2 border text-sm"
+                    className="py-4 text-start px-2 border text-sm"
                   >
                     {String(cell.renderValue() ?? "")}
                   </td>
